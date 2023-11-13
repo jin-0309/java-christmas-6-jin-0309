57d2ebba6de2f93f26e43d3bf3950f9bb798acca
김관진 <cozyschooll@gmail.com>
2023-11-13 16:42:01 +0900

    feat: user 클래스 생성

---
+++ src/main/java/christmas/model/User.java
@@ -0,0 +1,13 @@
+package christmas.model;
+
+public class User {
+    private int userID;
+
+    public User(int userID) {
+        this.userID = userID;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+}
