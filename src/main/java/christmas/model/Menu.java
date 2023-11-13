9229019136b6606cc513946ed6c03dcbc0818c8a
김관진 <cozyschooll@gmail.com>
2023-11-13 20:03:00 +0900

    refactor: Menu 클래스에서 record로 수정

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,29 +1,5 @@
 package christmas.model;
 
-public class Menu {
-
-    private final int menuId;
-
-    private final String name;
-
-    private final int price;
-
-    public Menu(int menuId, String name, int price) {
-        this.menuId = menuId;
-        this.name = name;
-        this.price = price;
-    }
-
-    public int getMenuId() {
-        return this.menuId;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
+public record Menu(int menuId, String name, int price) {
 
 }
